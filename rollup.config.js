5d7895aa63f2ccec1f1045c7777f34f8ed86d9b6
Daniel Ström <D@nielstrom.se>
2018-12-02 12:33:00 +0100

    Upgrade rollup and rollup-plugin-postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,30 @@
-import postcss from "rollup-plugin-postcss"
+import postcss from "rollup-plugin-postcss";
 
 export default [
   {
     input: "./src/index.js",
-    output: [
-      { file: "dist/extract.js", format: "es" }
-    ],
+    output: {
+      file: "dist/extract.js",
+      format: "es"
+    },
     plugins: [
       postcss({
         modules: true,
-        extract: true,
-      }),
+        extract: true
+      })
     ]
   },
   {
     input: "./src/index.js",
-    output: [
-      { file: "dist/inline.js", format: "es" }
-    ],
+    output: {
+      file: "dist/inline.js",
+      format: "es"
+    },
     plugins: [
       postcss({
         modules: true,
-        inline: true,
-      }),
+        inline: true
+      })
     ]
   }
-]
+];
